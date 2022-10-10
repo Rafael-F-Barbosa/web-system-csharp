e6f60d9a1bb049949b1feae5e9e28cbc21717f1a
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-10-10 18:35:43 -0300

    🔥 delete project

--- README.md
+++
@@ -1,2 +0,0 @@
-# web-system-csharp
-Minha implementação code along do curso de C# com Orientação à Objetos do Nélio Alves.
--- SalesWebMvc/Models/Department.cs
+++
@@ -1,6 +0,0 @@
-namespace SalesWebMvc.Models;
-public class Department
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
