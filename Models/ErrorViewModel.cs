73212fcdcf2198b27a205ff91cc1ab6eaf470fb6
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-10-12 13:38:48 -0300

    🔥 delete project again

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace web_system_csharp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
