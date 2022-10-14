eaa0fc73fe4f0ce4e04e96a86a4ae2b451b14883
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-10-14 15:05:37 -0300

    ✨ Sellers details

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using web_system_csharp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace web_system_csharp.Services
 {
@@ -27,7 +28,7 @@ namespace web_system_csharp.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj=>obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         
         public void Remove(int id)
