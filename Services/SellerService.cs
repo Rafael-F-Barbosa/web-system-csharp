8925fbebcb46d429fe890c1f9d94aa0dc2d31719
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-10-14 11:54:26 -0300

    ✨ delete seller

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -24,5 +24,18 @@ namespace web_system_csharp.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+
+        }
     }
 }
