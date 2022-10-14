5d13691b87b3eac738e0949f72f59801cf966d5b
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-10-14 15:37:50 -0300

    ✨ custom exception views

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using web_system_csharp.Services;
 using web_system_csharp.Models;
@@ -47,12 +48,12 @@ namespace web_system_csharp.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided."});
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not found."});
             }
             return View(obj);
         }
@@ -69,12 +70,12 @@ namespace web_system_csharp.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided."});
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not found."});
             }
             return View(obj);
         }
@@ -83,12 +84,12 @@ namespace web_system_csharp.Controllers
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided."});
             }
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not found."});
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -102,7 +103,7 @@ namespace web_system_csharp.Controllers
         {
             if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new {message = "Id mismatch."});
             }
             try
             {
@@ -111,14 +112,23 @@ namespace web_system_csharp.Controllers
             }
             catch( NotFoundException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = e.Message});
             }
             catch(DbConcurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new {message = e.Message});
             }
         }
 
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
+        }
 
     }
 }
--- Models/ViewModels/ErrorViewModel.cs
+++ Models/ViewModels/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace web_system_csharp.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
+    public string Message { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
